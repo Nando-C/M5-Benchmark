69b31399cbf3f9c497e6fe2401bf449abf6ec433
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-07-02 14:13:03 +0100

    adds basic CRUD for media

--- src/services/media/index.js
+++ src/services/media/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { getMediaArray, writeMedia } from '../../lib/fileSystemTools.js'
 
 const mediaRouter = express.Router()
 
@@ -14,9 +15,16 @@ const mediaRouter = express.Router()
 
 // POST Media =====================================================================
 
-mediaRouter.post("/", async ( req, res, next) => {
+mediaRouter.post("/", async( req, res, next) => {
     try {
+        const mediaArr = await getMediaArray()
         
+        const newMedia = req.body
+        mediaArr.push(newMedia)
+
+        await writeMedia(mediaArr)
+
+        res.status(201).send({newMedia})
     } catch (error) {
         next(error)
         
@@ -25,23 +33,81 @@ mediaRouter.post("/", async ( req, res, next) => {
 
 // GET Medias (list) (with reviews) ===============================================
 
-mediaRouter.get("/", async ( req, res, next) => {})
+mediaRouter.get("/", async ( req, res, next) => {
+    try {
+        const mediaArr = await getMediaArray()
+
+        res.send(mediaArr)
+    } catch (error) {
+        next(error)
+    }
+})
 
 // GET Media (single) (with reviews) ==============================================
 
-mediaRouter.get("/:id", async ( req, res, next) => {})
+mediaRouter.get("/:imdbID", async ( req, res, next) => {
+    try {
+        const mediaArr = await getMediaArray()
+        const media = mediaArr.find(media => media.imdbID === req.params.imdbID)
+        if (media) {
+            res.send(media)
+        } else {
+            next(createError(404, `Media with imdbID ${req.params.imdbID} not found!`))
+        }
+    } catch (error) {
+        next(error)
+    }
+})
 
 // UPDATE Media ===================================================================
 
-mediaRouter.put("/:id", async ( req, res, next) => {})
+mediaRouter.put("/:id", async ( req, res, next) => {
+    try {
+        const mediaArr = await getMediaArray()
+        const media = mediaArr.find(media => media.imdbID === req.params.imdbID)
+
+        if (media) {
+            const remainingMedia = mediaArr.filter(media => media.imdbID !== req.params.imdbID)
+
+            const modifiedMedia = { ...media, ...req.body}
+
+            remainingMedia.push(modifiedMedia)
+            await writeMedia(remainingMedia)
+
+            res.send(modifiedMedia)
+        } else {
+            next(createError(404, `Media with imdbID ${req.params.imdbID} not found!`))
+        }
+    } catch (error) {
+        next(error)
+    }
+})
+
+// DELETE Media ===================================================================
+
+mediaRouter.delete("/:id", async ( req, res, next) => {
+    try {
+        const mediaArr = await getMediaArray()
+        const media = mediaArr.find(media => media.imdbID === req.params.imdbID)
+
+        if (media) {
+            const remainingMedia = mediaArr.filter(media => media.imdbID !== req.params.imdbID)
+
+            await writeMedia(remainingMedia)
+
+            res.status(204).send()
+        } else {
+            next(createError(404, `Media with imdbID ${req.params.imdbID} not found!`))
+        }
+    } catch (error) {
+        next(error)
+    }
+})
 
 // POST Poster to single media ====================================================
 
 mediaRouter.post("/:id", async ( req, res, next) => {})
 
-// DELETE Media ===================================================================
-
-mediaRouter.delete("/:id", async ( req, res, next) => {})
 
 
 export default mediaRouter
